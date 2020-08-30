83913cd99f6e4f2660cc889e005e733be7853c1a
SORIK28850 <55577480+SORIK28850@users.noreply.github.com>
2020-08-30 12:36:55 +0200

    Update ModificarPersonal.vue

--- src/components/gestion/botones/ModificarPersonal.vue
+++ src/components/gestion/botones/ModificarPersonal.vue
@@ -305,7 +305,7 @@
              */
             usuarioCorrecto(){
                 let usuarioMaestro = false;
-                if(this.usuario.email === "ncreador@gmail.com"){
+                if(this.usuario.email === "@gmail.com"){
                     usuarioMaestro = true;
                 }
                 return usuarioMaestro;
